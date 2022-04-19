2d55d3d77482fc90f1000408562f1fa2fb6ede0f
Jacob Causey <cau16002@byui.edu>
2022-04-18 18:37:04 -0600

    makedankmemesnotwar

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ahhh You seek the grail? ");
+            Console.WriteLine("You must answer my riddles three! ");
+            Console.WriteLine("What is your name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine("What is your quest? ");
+            string quest = Console.ReadLine();
+            Console.WriteLine("What is your Favorite Color? ");
+            string color = Console.ReadLine();
+            Console.WriteLine("You may go.");
+        }
+    }
+}
