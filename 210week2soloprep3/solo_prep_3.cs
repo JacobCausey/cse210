22420ffc154383a3d9fa27d1a16a141418de19a0
Jacob Causey <cau16002@byui.edu>
2022-04-23 17:21:25 -0600

    trying to get the guess to work

--- 210week2soloprep3/solo_prep_3.cs
+++ 210week2soloprep3/solo_prep_3.cs
@@ -6,11 +6,14 @@ namespace solo_prep_3
     {
         static void Main(string[] args)
         {
+            //Create the Number
             Random randomGenerator = new Random();
-            int number = randomGenerator.Next(1, 11);
-            Console.WriteLine("Number is: " + number);
-
-            
+            int number = randomGenerator.Next(1, 101);
+            Console.WriteLine("What is the magic number?");
+            Console.WriteLine("What is your guess?");
+            string guess;
+            guess = Console.Readline();
+            int guessValue = int.Parse(guess);
         }
     }
 }
