fed61d2eb62fc8bae3e132ab9e4c1c060ad3a1db
Jacob Causey <cau16002@byui.edu>
2022-04-23 16:58:51 -0600

    SoloPrep3Step1

---
+++ 210week2soloprep3/solo_prep_3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace solo_prep_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomGenerator = new Random();
+            int number = randomGenerator.Next(1, 11);
+            Console.WriteLine("Number is: " + number);
+
+            
+        }
+    }
+}
