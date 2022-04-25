17f4acd4893ac2ea5a01c8b0d430994924e0a67d
Jacob Causey <cau16002@byui.edu>
2022-04-25 09:58:36 -0600

    Stillverypotato

--- 210week2soloprep4/solo_prep_4.cs
+++ 210week2soloprep4/solo_prep_4.cs
@@ -6,9 +6,14 @@ namespace solo_prep_4.cs
     {
         static void Main(string[] args)
         {
+            //Create list and begin torture, I mean program:
             List<int> numbers = new List<int>();
             Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+            // There needs to be a for or do loop to add numbers:
             Console.WriteLine("Enter Number: "); numbers.Add(int.Parse(Console.ReadLine()));
+
+            //Then I'll just use some writelines to drop the needed thingies:
             Console.WriteLine($"The Sum is: {numbers.Sum()}");
             Console.WriteLine($"The average is: {numbers.Average()}");
             Console.WriteLine($"The largest number is: {numbers.Max()}");
