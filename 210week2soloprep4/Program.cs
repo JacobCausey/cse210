fe3b339fb3653aad51c16cad9cf910568d4c9f8c
Jacob Causey <cau16002@byui.edu>
2022-04-25 09:19:09 -0600

    Week2Tatos

---
+++ 210week2soloprep4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Program
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        }
+    }
+}
