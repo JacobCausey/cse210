2a73f7f9cf43546732baac2dfdc5f0ea6d26fa9c
Jacob Causey <cau16002@byui.edu>
2022-04-30 16:15:03 -0600

    meh

--- 210week2Project1/ticTacTorture.cs
+++ 210week2Project1/ticTacTorture.cs
@@ -8,7 +8,7 @@ namespace ticTacTorture
         static void Main(string[] args)
         {
             List<string> spaces =GetSpaces();
-            string playerStatus = 'x';
+            string playerStatus = "x";
 
             while(!GameContinue(spaces))
             {
@@ -41,6 +41,73 @@ namespace ticTacTorture
                 Console.WriteLine($"  {spaces[6]}  |  {spaces[7]}  |  {spaces[8]}  |");
                 Console.WriteLine("     |     |      ");
             }
+            static bool IsItOver(List<string> spaces)
+            {
+                bool over = false;
+                if(IsWinner(spaces, "x")|| IsWinner(spaces, "o") || IsTie(spaces))
+                {
+                    over = true;
+                }
 
+                return over;
+            }
+
+            static bool IsWinner(List<string> board, string playerStatus)
+            {
+                bool isWinner = false;
+
+            if ((board[0] == playerStatus && board[1] == playerStatus && board[2] == playerStatus)
+                || (board[3] == playerStatus && board[4] == playerStatus && board[5] == playerStatus)
+                || (board[6] == playerStatus && board[7] == playerStatus && board[8] == playerStatus)
+                || (board[0] == playerStatus && board[3] == playerStatus && board[6] == playerStatus)
+                || (board[1] == playerStatus && board[4] == playerStatus && board[7] == playerStatus)
+                || (board[2] == playerStatus && board[5] == playerStatus && board[8] == playerStatus)
+                || (board[0] == playerStatus && board[4] == playerStatus && board[8] == playerStatus)
+                || (board[2] == playerStatus && board[4] == playerStatus && board[6] == playerStatus)
+                )
+            {
+                isWinner = true;
+            }
+
+            return isWinner; 
+            }
+            static bool IsTie(List<string> board)
+            {
+                bool foundSpaces = false;
+
+                foreach (string value in spaces)
+                {
+                    if(char.IsDigit(value[0]))
+                    {
+                        foundSpaces = true;
+                        break;
+                    }
+                }
+                return !foundSpaces;
+            }
+            static string GetNextPlayer(string playerStatus)
+            {
+                string nextPlayer = "x";
+
+                if (playerStatus == "x")
+                {
+                    nextPlayer = "o";
+                } 
+                return nextPlayer;
+            }
+            static int GetMoveChoice(string playerStatus)
+            {
+                Console.WriteLine($"{playerStatus} Where would you like to play? ");
+                string move_choice = Console.ReadLine();
+
+                int choice = int.Parse(move_choice);
+                return choice;
+            }
+            static void PlaceChoice(List<string> spaces, int choice, string playerStatus)
+            {
+                int index = choice -1;
+
+               spaces[index] = playerStatus;
+            }
     }
 }
